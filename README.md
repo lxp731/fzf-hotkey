663a2b4e0df56fc18f883eea813ccb51b531d7bb
Burgess Leo <liuxiaopeng731@gmail.com>
2025-01-18 20:52:25 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# fzf-hotkey
-A plugin for zsh integrate fzf.
+### fzf-hotkey
+
+A plugin for oh-my-zsh integrate fzf.
+
+### Installation
+
+```bash
+git clone https://github.com/lxp731/fzf-hotkey.git "$ZSH_CUSTOM/plugins/fzf-hotkey"
+```
+
+Then add this line to your `.zshrc`. Make sure it is before the line source `$ZSH/oh-my-zsh.sh`.
+```bash
+plugins=(
+    ...
+    fzf-hotkey
+)
+```
---
+++ fzf-hotkey.plugin.zsh
@@ -0,0 +1,45 @@
+# Load fzf integration
+source <(fzf --zsh)
+
+# CTRL-Y to copy the command into clipboard using pbcopy
+export FZF_CTRL_R_OPTS="
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --color header:italic
+  --header 'Press CTRL-Y to copy command into clipboard'
+  --height 80% --layout reverse"
+
+# Preview file content using bat (https://github.com/sharkdp/bat)
+export FZF_CTRL_T_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'batcat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'
+  --height 80% --layout reverse"
+
+# Print tree structure in the preview window
+export FZF_ALT_C_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'tree -C {}'
+  --height 80% --layout reverse"
+
+# ripgrep->fzf->vim [QUERY]
+rfv() (
+  RELOAD='reload:rg --column --color=always --smart-case {q} || :'
+  OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
+            vim {1} +{2}     # No selection. Open the current line in Vim.
+          else
+            vim +cw -q {+f}  # Build quickfix list for the selected items.
+          fi'
+  
+  fzf --disabled --ansi --multi \
+      --bind "start:$RELOAD" --bind "change:$RELOAD" \
+      --bind "enter:become:$OPENER" \
+      --bind "ctrl-o:execute:$OPENER" \
+      --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
+      --delimiter : \
+      --preview 'batcat --style=full --color=always --highlight-line {2} {1}' \
+      --preview-window '~4,+{2}+4/3,<80(up)' \
+      --query "$*"
+)
+
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd auto_activate
